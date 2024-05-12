ab16908a9d6dcf54ea096329359061a8a46d709e
JuanPF56 <juanpablofresia@gmail.com>
2024-05-12 16:37:37 -0300

    Cambiar orden de build para que los assets se copien al final

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-});
+})
