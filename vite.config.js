e9545385638de4a3172059c1fbe4d231926a2d36
JuanPF56 <juanpablofresia@gmail.com>
2024-05-12 17:16:13 -0300

    Arreglar configuracion de vite por cambio de nombre de repo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Worms-Website/',
   plugins: [react()],
   assetsInclude: ['src/assets/*.png', 'src/assets/*.svg', 'src/assets/*.pdf']
 })
